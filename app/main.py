353fe53136117a8c454e2b4dbc0ce41fcdd1a4ed
Akash <akash.krishnan.046@gmail.com>
2024-10-13 15:22:47 +0530

    added cors and gave access to all origins

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from app import models, auth, database
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Include authentication-related routes
 app.include_router(auth.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create the database tables
 @app.on_event("startup")
